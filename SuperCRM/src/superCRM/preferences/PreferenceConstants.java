0bd91d63a14b6160096983a1626e0531e9dc0821
code1990 <s1332177151@sina.com>
2020-05-26 15:21:13 +0800

    init

---
+++ SuperCRM/src/superCRM/preferences/PreferenceConstants.java
@@ -0,0 +1,15 @@
+package superCRM.preferences;
+
+public class PreferenceConstants {
+	/** 是否自动登录 */
+	public static final String P_AUTO_LOGIN = "autoLogin";
+
+	/** 用户名 */
+	public static final String P_USER_NAME = "cuiran";
+
+	/** 密码 */
+	public static final String P_PASSWORD = "123456";
+
+	/** 默认搜索类型 */
+	public static final String P_DEFAULT_SEARCH = "defaultSearch";
+}
